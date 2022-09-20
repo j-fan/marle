1bd1c91744f0a5ff3152dcdce06f5db07aedae05
CallumHoward <callumh@cse.unsw.edu.au>
2022-09-20 23:28:07 +1000

    Format and fix tailwind

--- callum/tailwind.config.cjs
+++ callum/tailwind.config.cjs
@@ -1,15 +1,8 @@
-// import konstaConfig config
-const konstaConfig = require("konsta/config");
-
-// wrap config with konstaConfig config
-module.exports = konstaConfig({
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
-  darkMode: "class", // or 'media'
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
-});
+};
