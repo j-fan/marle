4eb7123a7f7c4677dafeb3a229bfb0833bf39203
Jane Fan <jfan9988@gmail.com>
2022-09-27 23:22:27 +1000

    update index page

--- jane/src/routes/+page.svelte
+++ jane/src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<h1>Jane's Marle artworks. Nothing to see here</h1>
+<script>
+  import { base } from "$app/paths";
+</script>
+
+<h3>Index page of Jane Fan's Marle artworks.</h3>
+<br />
+<p>List of artworks</p>
+<a href="{base}/fabric-of-dreams">Fabric of dreams</a>
