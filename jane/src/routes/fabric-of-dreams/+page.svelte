9f2ea3e8ec52a990508ba4c1893a75fc2782c279
Jane Fan <jfan9988@gmail.com>
2022-09-28 22:59:22 +1000

    add images

--- jane/src/routes/fabric-of-dreams/+page.svelte
+++ jane/src/routes/fabric-of-dreams/+page.svelte
@@ -1,6 +1,32 @@
-<script>
+<script type="ts">
   import ChatBot from "$lib/components/ChatBot.svelte";
+  import { dreamImages, type DreamImageKey } from "./dream-images";
+
+  let currentImageKey: DreamImageKey = "bluebird";
+  $: currentImage = dreamImages[currentImageKey];
+
+  const goToImage = (key: DreamImageKey) => {
+    currentImage = dreamImages[key];
+  };
 </script>
 
-<p>fabric of dreams</p>
+<img
+  src={currentImage}
+  alt="neural network feature visualisation of blue birds"
+  on:click={() => {
+    goToImage("spots");
+  }}
+/>
+
 <ChatBot />
+
+<style>
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+</style>
