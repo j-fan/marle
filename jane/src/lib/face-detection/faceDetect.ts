2521e14e149296b6957ba487642c650197bdd063
Jane Fan <jfan9988@gmail.com>
2022-11-12 11:06:04 +1100

    add face expression and draw labels

--- jane/src/lib/face-detection/faceDetect.ts
+++ jane/src/lib/face-detection/faceDetect.ts
@@ -1,25 +1,11 @@
 import * as faceApi from "@vladmandic/face-api";
-import {
-  FaceDetection,
-  FaceLandmarks68,
-  Gender,
-  TinyFaceDetectorOptions,
-  type WithFaceLandmarks
-} from "@vladmandic/face-api";
+import { TinyFaceDetectorOptions } from "@vladmandic/face-api";
 
 export const WEBCAM_VIDEO_ID = "webcam-video";
-export const WEBCAM_CANVAS_ID = "webcam-canvas";
-export const FACE_DEBUG_CANVAS_ID = "face-debug";
+export const FACE_CANVAS_ID = "face-debug";
 
 let detector: TinyFaceDetectorOptions;
-let totalGender = 0;
-let totalAge = 0;
-let estimatedAge = 0;
-let estimatedGender = Gender.MALE;
-let successfulAgeGenderDetections = 0;
 let netsLoaded = false;
-
-const totalAgeGenderDetections = 20;
 
 type FaceDetectOptions = {
   showDebug?: boolean;
@@ -27,10 +13,24 @@ type FaceDetectOptions = {
   height: number;
 };
 
+type AllFaceData = faceApi.WithFaceExpressions<
+  faceApi.WithAge<
+    faceApi.WithGender<
+      faceApi.WithFaceLandmarks<
+        {
+          detection: faceApi.FaceDetection;
+        },
+        faceApi.FaceLandmarks68
+      >
+    >
+  >
+>;
+
 const startFaceDetect = async (): Promise<void> => {
   await faceApi.nets.tinyFaceDetector.loadFromUri("./models");
   await faceApi.nets.ageGenderNet.loadFromUri("./models");
   await faceApi.nets.faceLandmark68TinyNet.loadFromUri("./models");
+  await faceApi.nets.faceExpressionNet.loadFromUri("./models");
 
   const inputSize = 512;
   const scoreThreshold = 0.5;
@@ -41,83 +41,113 @@ const startFaceDetect = async (): Promise<void> => {
   netsLoaded = true;
 };
 
-const runFaceLandmarkDetection = async ({
-  showDebug,
-  width,
-  height
-}: FaceDetectOptions) => {
+const runDetections = async (
+  options: FaceDetectOptions
+): Promise<AllFaceData | undefined> => {
+  const t0 = performance.now();
+
+  if (!netsLoaded) {
+    return undefined;
+  }
+
   const result = await faceApi
     .detectSingleFace(WEBCAM_VIDEO_ID, detector)
-    .withFaceLandmarks(true);
+    .withFaceLandmarks(true)
+    .withAgeAndGender()
+    .withFaceExpressions();
 
-  const faceLandmarkDebugCanvas = document.getElementById(
-    FACE_DEBUG_CANVAS_ID
-  ) as HTMLCanvasElement;
-
-  if (showDebug && faceLandmarkDebugCanvas && result) {
+  if (result) {
+    const canvasElement = document.getElementById(
+      FACE_CANVAS_ID
+    ) as HTMLCanvasElement;
     const dimensions = faceApi.matchDimensions(
-      faceLandmarkDebugCanvas,
-      { width, height },
+      canvasElement,
+      { width: options.width, height: options.height },
       true
     );
     const resizedResults = faceApi.resizeResults(result, dimensions);
-    faceApi.draw.drawFaceLandmarks(faceLandmarkDebugCanvas, resizedResults);
+
+    const fps = 1000 / (performance.now() - t0);
+    drawFaceData(canvasElement, resizedResults, fps);
   }
+
   return result;
 };
 
-const runAgeGenderDetection = async ({ showDebug }: FaceDetectOptions) => {
-  const result = await faceApi
-    .detectSingleFace(WEBCAM_VIDEO_ID, detector)
-    .withAgeAndGender();
+// Taken from the face-api.js demo code: https://github.com/vladmandic/face-api/blob/master/demo/webcam.js
+function drawFaceData(
+  canvas: HTMLCanvasElement,
+  person: AllFaceData,
+  fps: number,
+  color = "#00FF00"
+) {
+  const ctx = canvas.getContext("2d");
+  if (!ctx) return;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  if (result && result.age > 0) {
-    successfulAgeGenderDetections++;
-    if (result.gender === Gender.FEMALE) {
-      totalGender += result.genderProbability;
-    } else {
-      totalGender -= result.genderProbability;
-    }
-    totalAge += result.age;
-    estimatedAge = totalAge / successfulAgeGenderDetections;
-    estimatedGender = totalGender > 0 ? Gender.FEMALE : Gender.MALE;
+  // draw box around each face
+  ctx.shadowColor = "black";
+  ctx.shadowBlur = 2;
+  ctx.lineWidth = 3;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+  ctx.globalAlpha = 0.6;
+  ctx.beginPath();
+  ctx.rect(
+    person.detection.box.x,
+    person.detection.box.y,
+    person.detection.box.width,
+    person.detection.box.height
+  );
+  ctx.stroke();
+  ctx.globalAlpha = 1;
+
+  // draw FPS
+  ctx.font = "20px monospace";
+  ctx.fillStyle = color;
+  ctx.fillText(`FPS: ${fps}`, 10, 25);
+
+  // draw text labels
+  const expression = Object.entries(person.expressions).sort(
+    (a, b) => b[1] - a[1]
+  );
+  ctx.fillStyle = color;
+  ctx.fillText(
+    `gender: ${Math.round(100 * person.genderProbability)}% ${person.gender}`,
+    person.detection.box.x,
+    person.detection.box.bottom + 74
+  );
+  ctx.fillText(
+    `expression: ${Math.round(100 * expression[0][1])}% ${expression[0][0]}`,
+    person.detection.box.x,
+    person.detection.box.bottom + 56
+  );
+  ctx.fillText(
+    `age: ${Math.round(person.age)} years`,
+    person.detection.box.x,
+    person.detection.box.bottom + 38
+  );
+  ctx.fillText(
+    `roll:${person.angle.roll}° pitch:${person.angle.pitch}° yaw:${person.angle.yaw}°`,
+    person.detection.box.x,
+    person.detection.box.bottom + 20
+  );
+
+  // draw face points for each face
+  ctx.globalAlpha = 0.8;
+  ctx.fillStyle = color;
+  const pointSize = 2;
+  for (let i = 0; i < person.landmarks.positions.length; i++) {
+    ctx.beginPath();
+    ctx.arc(
+      person.landmarks.positions[i].x,
+      person.landmarks.positions[i].y,
+      pointSize,
+      0,
+      2 * Math.PI
+    );
+    ctx.fill();
   }
+}
 
-  if (showDebug) {
-    console.log("estimated", estimatedAge, estimatedGender);
-    console.log("current detection", result?.age, result?.gender);
-  }
-};
-
-type DetectionResults = {
-  landmarks?: WithFaceLandmarks<{ detection: FaceDetection }, FaceLandmarks68>;
-  estimatedAge: number;
-  estimatedGender: Gender;
-  isFaceGenderReady: boolean;
-};
-
-const runDetections = async (
-  options: FaceDetectOptions
-): Promise<DetectionResults> => {
-  let isFaceGenderReady = false;
-
-  if (!netsLoaded) {
-    return { estimatedAge, estimatedGender, isFaceGenderReady };
-  }
-
-  if (successfulAgeGenderDetections < totalAgeGenderDetections) {
-    await runAgeGenderDetection(options);
-  } else {
-    isFaceGenderReady = true;
-  }
-  const landmarks = await runFaceLandmarkDetection(options);
-
-  return {
-    landmarks,
-    estimatedAge,
-    estimatedGender,
-    isFaceGenderReady
-  };
-};
-
-export { startFaceDetect, runDetections, estimatedAge, estimatedGender };
+export { startFaceDetect, runDetections, netsLoaded };
--- jane/src/routes/your-face/+page.svelte
+++ jane/src/routes/your-face/+page.svelte
@@ -1,9 +1,8 @@
 <script type="ts">
   import {
-    FACE_DEBUG_CANVAS_ID,
+    FACE_CANVAS_ID,
     runDetections,
     startFaceDetect,
-    WEBCAM_CANVAS_ID,
     WEBCAM_VIDEO_ID
   } from "$lib/face-detection/faceDetect";
   import { initCamera } from "$lib/face-detection/webcam";
@@ -11,6 +10,32 @@
 
   let isDetectionReady = false;
   let webcamRef: HTMLVideoElement | null;
+
+  const getCanvasDimensions = () => {
+    let height = 0;
+    let width = 0;
+
+    if (!webcamRef) {
+      return { height, width };
+    }
+
+    if (webcamRef.videoWidth > webcamRef.videoHeight) {
+      width = webcamRef.clientWidth;
+      height =
+        (webcamRef.clientWidth / webcamRef.videoWidth) * webcamRef.videoHeight;
+    } else {
+      height = webcamRef.height;
+      width =
+        (webcamRef.clientHeight / webcamRef.videoHeight) * webcamRef.videoWidth;
+    }
+
+    if (webcamRef.clientWidth < 700) {
+      width *= 2;
+      height *= 2;
+    }
+
+    return { height, width };
+  };
 
   onMount(async () => {
     await initCamera();
@@ -20,8 +45,7 @@
       frame = requestAnimationFrame(loop);
       if (isDetectionReady && webcamRef) {
         await runDetections({
-          height: webcamRef.clientHeight,
-          width: webcamRef.clientWidth,
+          ...getCanvasDimensions(),
           showDebug: true
         });
       }
@@ -49,18 +73,22 @@
     playsinline
     on:play={startDetection}
   />
-  <canvas id={FACE_DEBUG_CANVAS_ID} />
+  <canvas id={FACE_CANVAS_ID} />
 </div>
 
 <style>
   .container {
     position: relative;
+    width: 100%;
+    height: 100%;
   }
 
   canvas,
   video {
-    width: 100%;
-    height: 100%;
+    min-width: 100%;
+    min-height: 100%;
+    object-fit: cover;
+    object-position: center top;
   }
 
   canvas {
