3e896fb665eec34f7553cec3375f3ce5ffdabde3
Jane Fan <jfan9988@gmail.com>
2022-11-24 22:04:12 +1100

    create rotating particles

--- home/src/routes/+page.svelte
+++ home/src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import * as BABYLON from "babylonjs";
+  import { createNoise3D } from "simplex-noise";
 
   let canvasRef: HTMLCanvasElement | null = null;
+  const numParticles = 200;
 
   onMount(() => {
+    const noise3D = createNoise3D();
     const engine = new BABYLON.Engine(canvasRef, true);
     const scene = new BABYLON.Scene(engine);
 
@@ -12,7 +15,7 @@
     const camera = new BABYLON.ArcRotateCamera(
       "ArcRotateCamera",
       1,
-      0.8,
+      0,
       20,
       new BABYLON.Vector3(0, 0, 0),
       scene
@@ -25,9 +28,14 @@
       { size: 0.01 },
       scene
     );
+    const emitterType = new BABYLON.SphereParticleEmitter(10);
 
     // Create a particle system
-    var particleSystem = new BABYLON.ParticleSystem("particles", 200, scene);
+    const particleSystem = new BABYLON.ParticleSystem(
+      "particles",
+      numParticles,
+      scene
+    );
 
     //Texture of each particle
     particleSystem.particleTexture = new BABYLON.Texture(
@@ -36,7 +44,8 @@
     );
 
     // Where the particles come from
-    particleSystem.emitter = emitter; // the starting object, the emitter
+    particleSystem.emitter = emitter;
+    particleSystem.particleEmitterType = emitterType;
 
     // Colors of all particles
     particleSystem.color1 = new BABYLON.Color4(0.7, 0.8, 1.0, 1.0);
@@ -74,22 +83,56 @@
     particleSystem.updateSpeed = 0.005;
 
     // Noise
-    var noiseTexture = new BABYLON.NoiseProceduralTexture("perlin", 256, scene);
-    noiseTexture.animationSpeedFactor = 5;
-    noiseTexture.persistence = 2;
-    noiseTexture.brightness = 0.5;
-    noiseTexture.octaves = 2;
+    // const noiseTexture = new BABYLON.NoiseProceduralTexture(
+    //   "perlin",
+    //   256,
+    //   scene
+    // );
+    // noiseTexture.animationSpeedFactor = 5;
+    // noiseTexture.persistence = 2;
+    // noiseTexture.brightness = 0.5;
+    // noiseTexture.octaves = 2;
 
-    particleSystem.noiseTexture = noiseTexture;
-    particleSystem.noiseStrength = new BABYLON.Vector3(100, 100, 100);
+    // particleSystem.noiseTexture = noiseTexture;
+    // particleSystem.noiseStrength = new BABYLON.Vector3(100, 100, 100);
 
-    // particleSystem.updateFunction = (particles) => {
-    //   for (var index = 0; index < particles.length; index++) {
-    //     var particle = particles[index];
-    //     particle.position = new BABYLON.Vector3(1, 1, 0);
-    //     particles[index] = particle;
-    //   }
-    // };
+    const flowField: Array<BABYLON.Vector3> = [];
+
+    for (let x = 0; x < numParticles; x++) {
+      for (let y = 0; y < numParticles; y++) {
+        const angle = noise3D(x, y, 0);
+        const rotQuart = BABYLON.Quaternion.RotationAxis(BABYLON.Axis.Z, angle);
+        const vector = new BABYLON.Vector3(1, 1, 1);
+        vector.applyRotationQuaternionInPlace(rotQuart);
+        flowField.push(vector);
+      }
+    }
+
+    let particle: BABYLON.Particle;
+    let position: BABYLON.Vector3;
+
+    particleSystem.updateFunction = (particles) => {
+      for (let index = 0; index < particles.length; index++) {
+        particle = particles[index];
+        position = particle.position;
+
+        // Start in random position
+        if (particle.age === 0) {
+          particle.position.addInPlace(flowField[index]);
+        }
+
+        // rotate around Y axis and 0,0,0
+        const rotQuart = BABYLON.Quaternion.RotationAxis(BABYLON.Axis.Y, 0.01);
+        particle.position.applyRotationQuaternionInPlace(rotQuart);
+
+        // Pull back to the center
+        particle.position.addInPlace(
+          position.multiply(new BABYLON.Vector3(-0.002, -0.002, -0.002))
+        );
+
+        particle.age++;
+      }
+    };
 
     // Start the particle system
     particleSystem.start();
