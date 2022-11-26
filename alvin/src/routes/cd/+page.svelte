f66933e0a7aa92cfa38d9f2f9faf089e9f11d55e
Jane Fan <jfan9988@gmail.com>
2022-11-26 17:06:41 +1100

    add pages for the 3 illustrations

---
+++ alvin/src/routes/cd/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+</script>
+
+<div>
+  <img
+    src="https://raw.githubusercontent.com/j-fan/marle-images/main/alvin/cd.jpg"
+    alt="cd with a face in the reflection"
+  />
+</div>
+
+<style>
+  div {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem;
+  }
+
+  img {
+    width: 50vh;
+    max-width: 100%;
+    object-fit: contain;
+    aspect-ratio: 1457 / 1480;
+  }
+</style>
---
+++ alvin/src/routes/ipod/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+</script>
+
+<div>
+  <img
+    src="https://raw.githubusercontent.com/j-fan/marle-images/main/alvin/ipod.jpg"
+    alt="ipod with a host coming out of the screen"
+  />
+</div>
+
+<style>
+  div {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem;
+  }
+
+  img {
+    width: 50vh;
+    max-width: 100%;
+    object-fit: contain;
+    aspect-ratio: 1206 / 1409;
+  }
+</style>
