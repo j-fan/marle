f60663bdcc07507bb858d69235d4188e13d9764b
Jane Fan <jfan9988@gmail.com>
2022-11-26 18:02:10 +1100

    implement glyph for ipod

---
+++ alvin/src/lib/components/Glyph.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  export let href: string;
+  /**
+   * Valid values: 1 - 13
+   */
+  export let glyphId = 1;
+  /**
+   * Valid values: 1 - 100
+   */
+  export let xPos: number;
+  /**
+   * Valid values: 1 - 100
+   */
+  export let yPos: number;
+  $: src = `https://raw.githubusercontent.com/j-fan/marle-images/main/glyph/g${glyphId}.png`;
+  export let colour = "#00ff00";
+
+  let randomAnimDelay = -Math.random() * 3;
+</script>
+
+<a {href}>
+  <img
+    {src}
+    alt="gylph to click"
+    style="--colour: {colour}; --random-anim-delay: {randomAnimDelay}s; --x: {xPos}%; --y:{yPos}%;"
+  />
+</a>
+
+<style>
+  img {
+    position: fixed;
+    top: var(--x);
+    left: var(--y);
+    width: 70px;
+    height: 70px;
+    filter: drop-shadow(0 0 5px var(--colour));
+    cursor: pointer;
+    animation: float 3s linear infinite;
+    animation-delay: var(--random-anim-delay);
+    transition: width 0.3s ease, height 0.3s ease;
+  }
+
+  img:hover {
+    filter: drop-shadow(0 0 5px var(--colour))
+      drop-shadow(0 0 5px var(--colour));
+    width: 80px;
+    height: 80px;
+  }
+
+  @keyframes float {
+    0%,
+    100% {
+      transform: translateY(0) translateX(-50%);
+    }
+
+    50% {
+      transform: translateY(20px) translateX(-50%);
+    }
+  }
+</style>
--- alvin/src/routes/cd/+page.svelte
+++ alvin/src/routes/cd/+page.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
   import CenteredImage from "$lib/components/CenteredImage.svelte";
+  import Glyph from "$lib/components/Glyph.svelte";
 </script>
 
 <CenteredImage
   src="https://raw.githubusercontent.com/j-fan/marle-images/main/alvin/cd.jpg"
   alt="cd with a face in the reflection"
+/>
+
+<Glyph
+  href="https://j-fan.github.io/marle/alvin/ipod"
+  xPos={10}
+  yPos={50}
+  glyphId={1}
+/>
+<Glyph
+  href="https://j-fan.github.io/marle/alvin/rest-here"
+  xPos={80}
+  yPos={50}
+  glyphId={2}
 />
 
 <style>
